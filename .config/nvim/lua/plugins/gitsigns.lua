6e7a02421fb8e0c1b3e7480a1fa10922bf630593
sls <Solomonshulman13@gmail.com>
2025-08-27 10:54:25 -0400

    Added missing files

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,20 @@
+return {
+    "lewis6991/gitsigns.nvim",
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+    opts = {
+        signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        },
+        signs_staged = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        },
+      },
+}
